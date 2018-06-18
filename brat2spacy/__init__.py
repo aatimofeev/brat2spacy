23790ab57ca980b098a466735908400927bacbd2
aatimofeev <eisenbart@ya.ru>
2018-06-18 10:57:29 +0300

    added spacy2brat conversion

--- brat2spacy/__init__.py
+++ brat2spacy/__init__.py
@@ -61,3 +61,29 @@ def brat2spacy(tokenizer, ann, text):
     else:
         assert len(words) == len(tags)
         return GoldParse(doc, words=words, tags=tags, entities=offsets_from_biluo_tags(doc, tags)), text
+
+
+def spacy2brat(doc):
+    tokens = []
+    relations = []
+    for token in doc:
+        tokens.append("T{id}\t{tag} {start} {end}\t{text}".format(id=str(token.i),
+                                                                  start=token.idx,
+                                                                  end=token.idx + len(token),
+                                                                  tag=token.pos_,
+                                                                  text=token.text
+                                                                  ))
+        relations.append("R{id}\t{label}  Arg1:T{head} Arg2:T{dep}".format(id=str(token.i),
+                                                                           label=token.dep_,
+                                                                           head=token.head.i,
+                                                                           dep=token.i
+                                                                           ))
+
+    entities = []
+    for id_, entity in enumerate(doc.ents):
+        entities.append("T{id}\t{type} {start} {end}\t{text}".format(id=id_,
+                                                                     type=entity.label_,
+                                                                     start=entity.start_char,
+                                                                     end=entity.end_char,
+                                                                     text=entity.text))
+    return tokens, relations, entities
--- brat2spacy/tests/test_brat_conversion.py
+++ brat2spacy/tests/test_brat_conversion.py
@@ -1,6 +1,6 @@
 import spacy
 
-from brat2spacy import brat2spacy
+from brat2spacy import brat2spacy, spacy2brat
 
 
 def test_brat2spacy():
@@ -40,3 +40,19 @@ R13	IP Arg1:T5 Arg2:T14"""
     assert gold.heads[1] == gold.heads[-1] == 4
     assert gold.labels[4] == 'ROOT'
     assert gold.heads[4] == 4
+
+
+def test_spacy2brat():
+    import spacy
+    nlp = spacy.load('web_core_sm')
+    text = "Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that " \
+           "designs, develops, and sells consumer electronics, computer software, and online services. The " \
+           "company's hardware products include the iPhone smartphone, the iPad tablet computer, the Mac personal " \
+           "computer, the iPod portable media player, the Apple Watch smartwatch, the Apple TV digital media player, " \
+           "and the HomePod smart speaker. Apple's software includes the macOS and iOS operating systems, the iTunes " \
+           "media player, the Safari web browser, and the iLife and iWork creativity and productivity suites, as well" \
+           " as professional applications like Final Cut Pro, Logic Pro, and Xcode. Its online services include the " \
+           "iTunes Store, the iOS App Store and Mac App Store, Apple Music, and iCloud."
+    doc = nlp(text)
+    tokens, relations, entities = spacy2brat(doc)
+    return tokens, relations, entities
